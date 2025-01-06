0bf5694b7d91f1509969bf237877ec20920c4760
Sandeep Muhal <sandeepmuhal9982@gmail.com>
2025-01-06 09:39:13 +0530

    Update code

---
+++ Basic_Python_learn/Function/function.py
@@ -0,0 +1,13 @@
+# Q create a function to add two numbers and store result in 'result' variable.
+def add(a,b):
+    return a+b
+
+a=int(input("Enter a value:-"))
+b=int(input("Enter second value:-"))
+print(add(a,b))
+
+
+def Multiply(a,b):
+    print("Multiply of two number:-",a*b)
+
+print(Multiply(a,b))
